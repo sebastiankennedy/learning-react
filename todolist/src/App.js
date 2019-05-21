1fadf796112848e0413d1d02b4ed303818e2b0e7
Sebastian <sebastiankennedy@foxmail.com>
2019-05-21 15:31:04 +0800

    Initial commit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            list: [
+                {
+                    id: new Date().toDateString(),
+                    body: 'Hello World, React.'
+                }
+            ],
+            bodyInputValue: ''
+        };
+    }
+
+    handleAddButton = () => {
+        this.setState({
+            list: [
+                ...this.state.list,
+                {
+                    id: new Date().toDateString(),
+                    body: this.state.bodyInputValue
+                }
+            ],
+            bodyInputValue: ''
+        });
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            bodyInputValue: event.target.value
+        })
+    }
+
+    handleItemClick = (key) => {
+        const list = [...this.state.list];
+        list.splice(key, 1);
+        this.setState({list})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+                <div>
+                    <div>
+                        <input id="body"
+                               type="text"
+                               value={this.state.bodyInputValue}
+                               onChange={this.handleInputChange}/>
+                        <button onClick={this.handleAddButton}>Add</button>
+                    </div>
+                    <div>
+                        <ul>
+                            {
+                                this.state.list.map((item, key) => {
+                                    return <li key={key} onClick={this.handleItemClick.bind(this, key)}>{item.id} - {item.body}</li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
