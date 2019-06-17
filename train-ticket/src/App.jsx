3adacbf694125bf2d0c1c2ad9f833473d62ceb7b
Sebastian <sebastiankennedy@foxmail.com>
2019-06-17 21:08:26 +0800

    Effect Hooks - useEffect demo

--- train-ticket/src/App.jsx
+++ train-ticket/src/App.jsx
@@ -1,19 +1,48 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 
-class AppClass extends Component {
+class App2 extends Component {
     state = {
-        count: 0
+        count: 0,
+        size: {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        }
     };
 
+    onResize = () => {
+        this.setState({
+            size: {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight
+            }
+        })
+    };
+
+    componentDidMount() {
+        document.title = this.state.count;
+
+        window.addEventListener('resize', this.onResize, false);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        document.title = this.state.count;
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize, false);
+    }
+
     render() {
-        const {count} = this.state;
+        const {count, size} = this.state;
 
         return (
             <button type="button"
                     onClick={() => {
                         this.setState({count: this.state.count + 1})
                     }}
-            >Click ({count})
+            >
+                Click ({count})
+                Size: {size.width}X{size.height}
             </button>
         );
     }
@@ -21,13 +50,40 @@ class AppClass extends Component {
 
 function App() {
     const [count, setCount] = useState(0);
-    const [name, setName] = useState('Mike');
-    return(
+    const [size, setSize] = useState({
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
+    });
+
+    const onResize = () => {
+        setSize({
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        });
+    };
+
+    useEffect(() => {
+        document.title = count;
+    });
+
+    // 传递空数组，可以避免每次渲染后执行。如果数组每一项数值不变，只会渲染一次。
+    useEffect( () => {
+        window.addEventListener('resize', onResize, false);
+
+        return () => {
+            window.removeEventListener('resize', this.onResize, false);
+        }
+    }, []);
+
+    return (
         <button
             type="button"
-            onClick={ () => {setCount(count+1)}}
+            onClick={() => {
+                setCount(count + 1);
+            }}
         >
-            Click ({count}), name ({name})
+            Click ({count})
+            Size: {size.width}X{size.height}
         </button>
     );
 }
