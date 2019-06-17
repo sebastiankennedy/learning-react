121a15d29f7b3eae34537d8b23470b830355d2fe
Sebastian <sebastiankennedy@foxmail.com>
2019-06-17 23:20:47 +0800

    Ref Hooks - useRef demo

--- train-ticket/src/App.jsx
+++ train-ticket/src/App.jsx
@@ -1,16 +1,22 @@
-import React, {Component, useState, useMemo, memo, useCallback} from 'react';
+import React, {useState, useMemo, useCallback, useRef, useEffect} from 'react';
 
-const Counter = memo(function Counter(props) {
-    console.log('Counter render');
-
-    return (
-        <h1 onClick={props.onClick}>{props.count}</h1>
-    );
-});
+class Counter extends React.PureComponent{
+    speak(){
+        console.log(`now counter is: ${this.props.count}`);
+    }
+    render(){
+        const {props} = this;
+        return (
+            <h1 onClick={props.onClick}>{props.count}</h1>
+        );
+    }
+}
 
 function App(props) {
     const [count, setCount] = useState(0);
     const [clickCount, setClickCount] = useState(0);
+    const counterRef = useRef();
+    const it = useRef();
 
     // 传入空数组，只会运行一次；传入参数变化时，才会运行一次；
     const double = useMemo(() => {
@@ -21,7 +27,33 @@ function App(props) {
     const onClick = useCallback(() => {
         console.log('click');
         setClickCount(clickCount + 1);
+
+        counterRef.current.speak();
     }, [],);
+
+    /* 不会停止，每次 App 重新渲染都会重新声明 it
+    useEffect(() => {
+        it = setInterval( () => {
+            setCount(count => count + 1);
+        }, 1000)
+    }, []);
+
+    useEffect( () => {
+       if (count >= 10) {
+           clearInterval(it);
+       }
+    });*/
+    useEffect(() => {
+        it.current = setInterval( () => {
+            setCount(count => count + 1);
+        }, 1000)
+    }, []);
+
+    useEffect( () => {
+        if (count >= 10) {
+            clearInterval(it.current);
+        }
+    });
 
     return (
         <div>
