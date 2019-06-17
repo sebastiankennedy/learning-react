0ab332b693a1dd05df461710ea02a01899895e7d
Sebastian <sebastiankennedy@foxmail.com>
2019-06-17 14:17:44 +0800

    Memo usage

--- train-ticket/src/App.jsx
+++ train-ticket/src/App.jsx
@@ -1,33 +1,28 @@
-import React, { Component, PureComponent, memo } from 'react';
+import React, { Component, memo } from 'react';
 import './App.css';
 
-// PureComponent 提供了简单的对比算法，来避免组件重新渲染，节省开销
-class Foo extends PureComponent{
-    // shouldComponentUpdate 实现效果与 PureComponent 一致
-    /*shouldComponentUpdate(nextProps, nextState, nextContext) {
-        if (nextProps.name === this.props.name){
-            return false;
-        }
-
-        return true;
-    }*/
-
-    render() {
-        // 监视 Foo 组件渲染情况
-        console.log('Foo render');
-        return null;
-    }
-}
+const Foo = memo(function Foo(props) {
+    // 监视 Foo 组件渲染情况
+    console.log('Foo render');
+    return <div>{props.person.age}</div>;
+});
 
 class App extends Component {
     state = {
-        count: 0
+        count: 0,
+        person: {
+            age: 1,
+        }
     };
     render() {
+        const person = this.state.person;
         return (
             <div>
-                <button onClick={ () => this.setState({ count: this.state.count + 1})}>add</button>
-                <Foo name="Mike"/>
+                <button onClick={ () => {
+                    person.age++;
+                    this.setState({ person })
+                }}>Add</button>
+                <Foo person={person} />
             </div>
         );
     }
