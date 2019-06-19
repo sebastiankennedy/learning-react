b456622d07b7eb650dbf05ef7349cf6c2ed72fe5
Sebastian <sebastiankennedy@foxmail.com>
2019-06-19 15:37:49 +0800

    React Component Compare - lifecycle

--- react-component-compare/src/App.js
+++ react-component-compare/src/App.js
@@ -1,27 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 class Hello extends React.Component {
     state = {
-        title: "Hello"
+        title: "Hello",
     };
 
+    componentDidMount() {
+        this.setState({title: "Big"})
+    }
+
     render() {
+        const {world, count} = this.props;
         const {title} = this.state;
-        const {world} = this.props;
 
         return (
-            <h1>{title} {world}</h1>
+            <h1>{title} {world} - {count}</h1>
         );
     }
 }
 
 function World(props) {
+    const {hello, count} = props;
     const [title, setTitle] = useState("World");
-    const {hello} = props;
+
+    // useEffect 传递空数组，相当于 componentDidMount()
+    useEffect(() => {
+        setTitle('React');
+    }, []);
 
     return (
-        <h1>{hello} {title}</h1>
+        <h1>{hello} {title} - {count}</h1>
     );
 }
 
@@ -30,17 +39,18 @@ class App extends React.Component {
         super(props);
         this.state = {
             hello: "Hello",
-            world: "World"
+            world: "World",
+            count: 10
         }
     }
 
     render() {
-        const {hello, world} = this.state;
+        const {hello, world, count} = this.state;
 
         return (
             <div>
-                <Hello world={world}/>
-                <World hello={hello}/>
+                <Hello world={world} count={count}/>
+                <World hello={hello} count={count}/>
             </div>
         );
     }
