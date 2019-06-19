529f7d0ee2dc7d33088d5c2f8249f12a51d56b04
Sebastian <sebastiankennedy@foxmail.com>
2019-06-19 15:02:04 +0800

    React Component Compare - state

--- react-component-compare/src/App.js
+++ react-component-compare/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 class Hello extends React.Component {
+    state = {
+        title: "Hello"
+    };
+
     render() {
+        const {title} = this.state;
+
         return (
-            <h1>Hello</h1>
+            <h1>{title}</h1>
         );
     }
 }
 
 function World() {
+    const [title, setTitle] = useState("World");
+
     return (
-        <h1>World</h1>
+        <h1>{title}</h1>
     );
 }
 
