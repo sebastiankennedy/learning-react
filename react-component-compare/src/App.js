187222df2c4826cbbbce497472aed24f7248cddc
Sebastian <sebastiankennedy@foxmail.com>
2019-06-19 14:49:35 +0800

    React Component Compare - simple

--- react-component-compare/src/App.js
+++ react-component-compare/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class Hello extends React.Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+        );
+    }
+}
+
+function World() {
+    return (
+        <h1>World</h1>
+    );
+}
+
+
 function App() {
-  return null;
+    return (
+        <div>
+            <Hello/>
+            <World/>
+        </div>
+    );
 }
 
 export default App;
