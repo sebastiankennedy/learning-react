3b5f4d996f518c14a1ec4b75a74990eff3395bbd
Sebastian <sebastiankennedy@foxmail.com>
2019-06-21 15:52:26 +0800

    React Component Compare - dispatch reducer - 2

--- react-component-compare/src/actions.js
+++ react-component-compare/src/actions.js
@@ -1,12 +1,12 @@
 // Action 是把所有业务操作集中管理起来
-export function createAddCount(payload){
+export function createAddCount(payload) {
     return {
         type: 'addCount',
         payload,
     }
 }
 
-export function createSubCount(payload){
+export function createSubCount(payload) {
     return {
         type: 'subCount',
         payload
