09173b2fb9893c1108be5aaffe22bdea4555ec04
Sebastian <sebastiankennedy@foxmail.com>
2019-06-23 17:03:12 +0800

    Train ticket index module - Header

--- train-ticket/src/index/App.jsx
+++ train-ticket/src/index/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {connect} from 'react-redux';
 import './App.css';
 
@@ -9,9 +9,15 @@ import Journey from './Journey';
 import Submit from './Submit';
 
 function App(props) {
+    const onBack = useCallback(() => {
+        window.history.back();
+    }, []);
+
     return (
         <div>
-            <Header/>
+            <div className="header-wrapper">
+                <Header title="火车票" onBack={onBack}/>
+            </div>
             <Journey/>
             <DepartDate/>
             <HighSpeed/>
